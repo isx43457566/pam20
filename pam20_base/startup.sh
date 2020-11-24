8bf5772b6472bef35de6de4bdbdb7cbdb3568709
RobertoMartinez <romarco1983@gmail.com>
2020-11-24 20:00:45 +0100

    pam base files

---
+++ pam20_base/startup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+useradd anna
+useradd pere
+useradd pau
+echo "anna" | passwd --stdin anna
+echo "pere" | passwd --stdin pere
+echo "pau" | passwd --stdin pau
+
+cp /opt/docker/login.defs /etc/login.defs
+/bin/bash
