70a33096a1dd42a8ecde2e7a830b82209e74475c
RobertoMartinez <romarco1983@gmail.com>
2020-11-24 20:21:56 +0100

    pam files

--- pam20_base/install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-useradd anna
-useradd pere
-useradd pau
-echo "anna" | passwd --stdin anna
-echo "pere" | passwd --stdin pere
-echo "pau" | passwd --stdin pau
-cp /opt/docker/login.defs /etc/login.defs
--- pam20_base/startup.sh
+++ pam20_base/startup.sh
@@ -5,6 +5,4 @@ useradd pau
 echo "anna" | passwd --stdin anna
 echo "pere" | passwd --stdin pere
 echo "pau" | passwd --stdin pau
-
 cp /opt/docker/login.defs /etc/login.defs
-/bin/bash
